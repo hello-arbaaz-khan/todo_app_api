15119c3a7024f69a579953ea8ba6367303f67613
hello-arbaaz-khan <hello.arbaaz.khan@gmail.com>
2025-11-04 12:49:55 +0500

    Allow host and other settings

--- todoapp/settings.py
+++ todoapp/settings.py
@@ -25,8 +25,7 @@ SECRET_KEY = 'django-insecure-qn3*90k+jw1e56sp(g+vwc_wwq5l8vd!ykfh34%3@ze-q81iez
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
@@ -124,7 +123,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
