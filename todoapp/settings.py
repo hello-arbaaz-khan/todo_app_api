b0388a2c4c5b27aaeeb989fae2b45ae3f6d039f9
hello-arbaaz-khan <hello.arbaaz.khan@gmail.com>
2025-11-05 11:41:19 +0500

    enabled cors and csrf for railway

--- todoapp/settings.py
+++ todoapp/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     "users",
+    'corsheaders',
     'rest_framework',
     'rest_framework_simplejwt',
 ]
@@ -48,6 +49,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -55,6 +57,8 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+CORS_ALLOW_ALL_ORIGINS = True
+CSRF_TRUSTED_ORIGINS = ['https://todoappapi-production-6fe1.up.railway.app']
 ROOT_URLCONF = 'todoapp.urls'
  
 TEMPLATES = [
